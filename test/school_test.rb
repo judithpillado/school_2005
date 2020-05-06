f4dac9527a2aecc25a90a5c7991413981faf748d
Judith Pillado <judith.pillado@gmail.com>
2020-05-06 15:03:44 -0500

    Add convert end time to clock time

--- test/school_test.rb
+++ test/school_test.rb
@@ -61,6 +61,12 @@ class SchoolTest < Minitest::Test
     school.add_student_name('megan')
 
     assert_equal ["Aurora", "Tim", "Megan"], school.standard_student_names
+  end
+
+  def test_convert_end_time_to_clock_time
+    school = School.new('9:00', 7)
+
+    assert_equal "4:00", school.convert_end_time_to_clock_time
   end 
 
 end
